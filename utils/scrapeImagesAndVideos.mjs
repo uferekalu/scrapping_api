280ae6dd357b82b592b3ca1b44b18833348905ec
Kalu Ufere <dekalusha@gmail.com>
2024-04-03 14:21:51 +0100

    modified

--- utils/scrapeImagesAndVideos.mjs
+++ utils/scrapeImagesAndVideos.mjs
@@ -44,10 +44,11 @@ export async function scrapeImagesAndVideos(url) {
     $('a').each((index, element) => {
       const href = $(element).attr('href')
       if (
-        href &&
-        (href.includes('.mp4') ||
-          href.includes('.mov') ||
-          href.includes('.wmv'))
+        (href &&
+          (href.includes('.mp4') ||
+            href.includes('.mov') ||
+            href.includes('.wmv'))) ||
+        href.includes('.webm')
       ) {
         videos.push(href)
       }
